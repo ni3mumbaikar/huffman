acd11a4348e8dc0f026b7a11c065d67e66c36959
Nitin Kale <nitinkale035@gmail.com>
2020-11-23 02:05:31 +0530

    Initial Commit

---
+++ NodeComparator.java
@@ -0,0 +1,11 @@
+
+import java.util.Comparator;
+
+public class NodeComparator implements Comparator<Node> {
+
+    @Override
+    public int compare(Node a, Node b) {
+        return a.getFreq() - b.getFreq();
+    }
+
+}
